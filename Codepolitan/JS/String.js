62991ee63dde75f47c65b6cab7d41627adc59e7c
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 11:14:23 +0700

    String Method

--- Codepolitan/JS/String.js
+++ Codepolitan/JS/String.js
@@ -5,3 +5,10 @@ let namaLengkap = namaDepan + " " + namaBelakang;
 console.log(namaLengkap); // Output: Tegar Kusuma
 console.log(namaLengkap.length); // Output: 12
 console.log(namaLengkap[6]);
+
+// String Method
+console.log(namaLengkap.toUpperCase()); // Output: TEGAR KUSUMA
+console.log(namaLengkap.toLowerCase()); // Output: tegar kusuma
+
+let nama = "   Tegar Kusuma   ";
+console.log(nama.trim().toLowerCase()); // Output: Tegar Kusuma
