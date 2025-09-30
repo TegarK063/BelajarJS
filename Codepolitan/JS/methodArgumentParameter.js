23e65fc54eae984dfa0746fd64e5051dfa3e62e4
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 11:23:29 +0700

    Method Argument Parameter

---
+++ Codepolitan/JS/methodArgumentParameter.js
@@ -0,0 +1,4 @@
+let nama = "Tegar Kusuma";
+console.log(nama.indexOf("Kusuma")); // dimulai dari index 6
+console.log(nama.slice(0, 7)); // dimulai dari index 0 sampai 7 (tidak termasuk 7)
+console.log(nama.replace("Tegar", "Budi")); // mengganti Tegar menjadi Budi
