3c76e8e72a46a3fa4de1096e9218bb4c595be677
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 11:06:27 +0700

    Concat  = penggabungan string dan mengenal index

---
+++ Codepolitan/JS/String.js
@@ -0,0 +1,7 @@
+let namaDepan = "Tegar";
+let namaBelakang = "Kusuma";
+
+let namaLengkap = namaDepan + " " + namaBelakang;
+console.log(namaLengkap); // Output: Tegar Kusuma
+console.log(namaLengkap.length); // Output: 12
+console.log(namaLengkap[6]);
