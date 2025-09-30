ff650e0906f00970cf24616c3948e4bcebeeb56e
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 10:19:51 +0700

    Pengenalan Variable

---
+++ Codepolitan/JS/variable.js
@@ -0,0 +1,7 @@
+let tahunMerdeka = 1945; // variable tahunMerdeka menyimpan nilai tahun kemerdekaan Indonesia
+
+console.log(tahunMerdeka); // menampilkan nilai tahunMerdeka ke konsol
+
+let sekarang = 2025;
+let umurIndonesia = sekarang - tahunMerdeka; // menghitung umur Indonesia
+console.log(`Umur Indonesia pada tahun  ${sekarang} adalah ${umurIndonesia} tahun.`); // menampilkan umur Indonesia pada tahun sekarang
