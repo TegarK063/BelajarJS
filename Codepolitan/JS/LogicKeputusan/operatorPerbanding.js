0830acd97c3e0c7d8e45d948ef88b732465aaaf9
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 13:24:48 +0700

    Update comments for comparison operators to clarify type requirements

--- Codepolitan/JS/LogicKeputusan/operatorPerbanding.js
+++ Codepolitan/JS/LogicKeputusan/operatorPerbanding.js
@@ -6,11 +6,11 @@ hasil = 2 >= 3;
 console.log(hasil)
 hasil = 2 >= 2;
 console.log(hasil)
-hasil = 5 == 5;
+hasil = 5 == 5; // ga harus satu type
 console.log(hasil)
 hasil = 5 != 5;
 console.log(hasil)
-hasil = 5 === "5";
+hasil = 5 === "5"; // Harus satu type yang sama
 console.log(hasil)
-hasil = "5" === "5";
+hasil = "5" === "5"; // Harus satu type yang sama
 console.log(hasil)
