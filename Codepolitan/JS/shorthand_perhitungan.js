8391aeec904085f595a8e101cbd4ad1dd5cad719
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 10:29:36 +0700

    Manipulasi number

---
+++ Codepolitan/JS/shorthand_perhitungan.js
@@ -0,0 +1,20 @@
+let score = 0;
+score += 1;
+console.log(`score = ${score}`);
+score -= 1;
+console.log(`score = ${score}`);
+score += 5;
+console.log(`score = ${score}`);
+score *= 2;
+console.log(`score = ${score}`);
+score /= 2;
+console.log(`score = ${score}`);
+score %= 2;
+console.log(`score = ${score}`);
+
+// increment (++)
+score++;
+console.log(`increment = ${score}`);
+// decrement (--)
+score--;
+console.log(`decrement = ${score}`);
