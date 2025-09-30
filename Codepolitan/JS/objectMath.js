5b88e1a764b0c28b28097237b5712c5bb22c94eb
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 11:45:31 +0700

    Objecct MAth

---
+++ Codepolitan/JS/objectMath.js
@@ -0,0 +1,12 @@
+Math.PI
+console.log(Math.PI)
+Math.E
+console.log(Math.E)
+Math.abs(-777)
+console.log(Math.abs(-777))
+Math.floor(4.8) // dibulatkan ke bawah = 4
+console.log(Math.floor(4.8))
+Math.round(4.8) // dibulatkan ke atas = 5
+console.log(Math.round(4.8))
+Math.random() // random
+console.log(Math.random())
