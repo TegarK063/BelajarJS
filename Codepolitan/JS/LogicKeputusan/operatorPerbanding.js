0ddb3efd202eac050a3582ff8f828e5bf85b95f6
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-09-30 13:20:49 +0700

    Operrator Perbandingan

---
+++ Codepolitan/JS/LogicKeputusan/operatorPerbanding.js
@@ -0,0 +1,16 @@
+let hasil = 10 > 2;
+console.log(hasil)
+hasil = 10 < 2;
+console.log(hasil)
+hasil = 2 >= 3;
+console.log(hasil)
+hasil = 2 >= 2;
+console.log(hasil)
+hasil = 5 == 5;
+console.log(hasil)
+hasil = 5 != 5;
+console.log(hasil)
+hasil = 5 === "5";
+console.log(hasil)
+hasil = "5" === "5";
+console.log(hasil)
