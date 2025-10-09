d39d7e65504788f3ed8e275ecf4a313fb39445cb
Tegar Kusuma <tegarkusuma063@gmail.com>
2025-10-09 21:08:52 +0700

    Belajar Switch Case

---
+++ Codepolitan/JS/LogicKeputusan/Switch.js
@@ -0,0 +1,15 @@
+let trafficLamp = 'Hijau';
+
+switch (trafficLamp) {
+  case 'Merah':
+    console.log('Berhenti');
+    break;
+  case 'Kuning':
+    console.log('Hati Hati');
+    break;
+  case 'Hijau':
+    console.log('Jalan');
+    break;
+  default:
+    console.log('Bukan Bagian dari Traffic Lamp');
+}
